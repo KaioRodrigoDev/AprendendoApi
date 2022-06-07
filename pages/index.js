5f5de13f13661f4d9497721774b61dd651c69256
Kaio Rodrigo <kaiorodrigocoder@gmail.com>
2022-06-07 19:18:57 -0300

    Aprendendo a usar Dato

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,35 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    async function Post() {
+      const token = '53de0084c550832c02357490bdb290'
+      const res = await fetch('https://graphql.datocms.com/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          query: '{ allProdutos { nomeproduto, valorproduto } }'
+        })
+      })
+        .then(res => res.json())
+        .then(res => {
+          console.log(res.data)
+          setPosts(res.data.allProdutos)
+        })
+    }
+
+    Post()
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,44 +39,19 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.nomeproduto}</h2>
+            <p>{post.valorproduto}</p>
+          </div>
+        ))}
       </main>
 
       <footer className={styles.footer}>
